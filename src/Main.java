f1c80c800ea44ef04588a748f3b614b70d3cd3e9
jose <jose.pinos@epn.edu.ec>
2022-11-08 11:27:48 -0500

    mensaje hola mundo e imprimir numeros desde 1...10

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("Hola mundo, este proyecto ya está inicializado con github");
+
+        int i = 1;
+        while(i <= 10){
+            System.out.print(i + " ");
+            i+=1;
+        }
+
+    }
+}
